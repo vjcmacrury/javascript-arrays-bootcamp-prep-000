57bc9a1fcfcaef92de49cdf408af1389b7b733eb
vjcmacrury@gmail.com MacRury <vjcmacrury@gmail.com>
2018-05-20 09:34:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,19 +36,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array6) {
 }
 
 var array7 = [7, 8, 9]
-function removeElementFromBeginningOfArray(array7) {
+function removeElementFromBeginningOfArray(array8) {
   array8 = array7.slice(1)
   return(array8)
 }
 
-var array9 = [10, 11, 12]
-function destructivelyRemoveElementFromEndOfArray(array9) {
-  array9.pop()
-  return(array9)
-}
-
-var array10 = [13, 14, 15]
-function removeElementFromEndOfArray(array10) {
-  array11 = array10.slice(0, array10.length - 1)
-  return(array11)
-}
